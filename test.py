c2d788a3556b86bf1294a3b9fa90786575cef63e
sho200 <86586955+sho200@users.noreply.github.com>
2021-09-23 02:43:51 +0900

    Update test.py

--- test.py
+++ test.py
@@ -7,5 +7,11 @@ app = Flask(__name__)
 def top_page():
     return 'Here is root page.'
 
+@app.route('/callback', methods=['POST'])
+def callback_post():
+    dt_now = datetime.datetime.now()
+    t =dt_now.strftime('%Y年%m月%d日\n%H時%M分%S秒')
+    return 'OK'
+
 if __name__ == '__main__':
     app.run()
